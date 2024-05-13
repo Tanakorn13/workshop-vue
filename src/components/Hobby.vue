ea6a80aba9c8c6038968e4fb758b2e530d6fac56
Tanakorn Pongtongmuang <96484902+Tanakorn13@users.noreply.github.com>
2024-05-13 10:35:00 +0700

    add hobby card

---
+++ src/components/Hobby.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="600"
+    style="
+      border: solid 2px red;
+      border-radius: 10px;
+      box-shadow: 10px 5px 5px red;
+    "
+  >
+    <v-img
+      src="../assets/low-angle-view-unrecognizable-muscular-build-man-preparing-lifting-barbell-health-club.jpg"
+      height="250px"
+    ></v-img>
+
+    <v-card-title class="d-flex justify-center orange--text">
+      Hobby
+    </v-card-title>
+
+    <v-card-actions>
+      <v-btn color="orange lighten-2" text> Explore </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text class="d-flex align-center flex-column">
+          <div class="d-flex align-center font-weight-semi-bold text-h5 mr-2">
+            <v-icon>mdi-dumbbell</v-icon>
+            <h6 class="ml-1 mr-3">Gym</h6>
+          </div>
+          <div class="d-flex align-center font-weight-semi-bold text-h5">
+            <v-icon>mdi-controller</v-icon>
+            <h6 class="ml-1">Game</h6>
+          </div>
+          <div class="d-flex align-center font-weight-semi-bold text-h5">
+            <v-icon>mdi-headphones</v-icon>
+            <h6 class="ml-1">Music</h6>
+          </div>
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HobbyCard",
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
+
+<style></style>
--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,69 +1,84 @@
 <template>
   <div class="container">
-    <v-card
-      class="mx-auto"
-      max-width="600"
-      style="
-        border: solid 2px red;
-        border-radius: 10px;
-        box-shadow: 10px 5px 5px red;
-      "
-    >
-      <div class="d-flex justify-center">
-        <v-avatar color="primary" size="200" style="margin-top: 20px">
-          <v-img
-            src="../assets/Twitter_SrankSungJinwoo.png"
-            height="200px"
-          ></v-img>
-        </v-avatar>
-      </div>
+    <v-row>
+      <v-col cols="6">
+        <v-card
+          class="mx-auto"
+          max-width="600"
+          style="
+            border: solid 2px red;
+            border-radius: 10px;
+            box-shadow: 10px 5px 5px red;
+          "
+        >
+          <div class="d-flex justify-center">
+            <v-avatar color="primary" size="200" style="margin-top: 20px">
+              <v-img
+                src="../assets/Twitter_SrankSungJinwoo.png"
+                height="200px"
+              ></v-img>
+            </v-avatar>
+          </div>
 
-      <v-card-title class="d-flex justify-center">
-        ธนากร พงค์ทองเมือง
-      </v-card-title>
+          <v-card-title class="d-flex justify-center orange--text">
+            ธนากร พงค์ทองเมือง
+          </v-card-title>
 
-      <v-card-subtitle class="d-flex justify-center"> หนุ่ม </v-card-subtitle>
+          <v-card-subtitle class="d-flex justify-center orange--text">
+            หนุ่ม
+          </v-card-subtitle>
 
-      <v-card-actions>
-        <v-btn color="orange lighten-2" text> Profile </v-btn>
+          <v-card-actions>
+            <v-btn color="orange lighten-2" text> Profile </v-btn>
 
-        <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-        <v-btn icon @click="show = !show">
-          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-        </v-btn>
-      </v-card-actions>
+            <v-btn icon @click="show = !show">
+              <v-icon>{{
+                show ? "mdi-chevron-up" : "mdi-chevron-down"
+              }}</v-icon>
+            </v-btn>
+          </v-card-actions>
 
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
 
-          <v-card-text>
-            <div
-              class="d-flex justify-center align-center font-weight-semi-bold text-h5"
-            >
-              <h6>
-                ชื่อ: ธนากร พงค์ทองเมือง <br />
-                ชื่อเล่น: หนุ่ม <br />
-                อายุ: 23 ปี <br />
-                คณะ: วิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์ <br />
-                มหาวิทยาลัย: มหาวิทยาลัยสงขลานครินทร์ <br />
-                งานอดิเรก: ฟังเพลง เล่นเกม เข้าฟิตเนส <br />
-              </h6>
+              <v-card-text>
+                <div
+                  class="d-flex justify-center align-center font-weight-semi-bold text-h5"
+                >
+                  <h6>
+                    ชื่อ: ธนากร พงค์ทองเมือง <br />
+                    ชื่อเล่น: หนุ่ม <br />
+                    อายุ: 23 ปี <br />
+                    คณะ: วิศวกรรมศาสตร์ สาขาวิศวกรรมคอมพิวเตอร์ <br />
+                    มหาวิทยาลัย: มหาวิทยาลัยสงขลานครินทร์ <br />
+                    <!-- งานอดิเรก: ฟังเพลง เล่นเกม เข้าฟิตเนส <br /> -->
+                  </h6>
+                </div>
+              </v-card-text>
             </div>
-          </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+      <v-col cols="6"> <HobbyCard /> </v-col>
+    </v-row>
+
+    <!-- <HobbyView /> -->
   </div>
 </template>
 
 <script>
+import HobbyCard from "@/components/Hobby.vue";
+// import TestCard from "@/components/TestCard.vue";
+
 export default {
   name: "MeView",
   data: () => ({
     show: false,
   }),
+  components: { HobbyCard },
 };
 </script>
 
